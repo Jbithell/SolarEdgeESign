9b256afd10135436c77b95e653a84455ec0fd7f1
James Bithell <hi@jbithell.com>
2017-07-19 21:58:26 +0100

    Correct scope issues

--- main.js
+++ main.js
@@ -1,45 +1,4 @@
-//Get function
-(function($) {
-    $.QueryString = (function(paramsArray) {
-        let params = {};
-
-        for (let i = 0; i < paramsArray.length; ++i)
-        {
-            let param = paramsArray[i]
-                .split('=', 2);
-
-            if (param.length !== 2)
-                continue;
-
-            params[param[0]] = decodeURIComponent(param[1].replace(/\+/g, " "));
-        }
-
-        return params;
-    })(window.location.search.substr(1).split('&'))
-})(jQuery);
-
-//Rounding Function
-function round(value, precision) {
-    var multiplier = Math.pow(10, precision || 0);
-    return Math.round(value * multiplier) / multiplier;
-}
-
-//Data conversion function
-var convertdata = function(data) {
-    if (data < 1001) {
-        return round(data,0) + "Wh";
-    } else if (data > 1000000) {
-        return round(data/1000000,1) + "MWh";
-    } else {
-        return round(data/1000,1) + "kWh";
-    }
-};
-
-
-//Begin code
-var sitecode = $.QueryString["code"];
-var apikey = $.QueryString["key"];
-
+var sitecode, apikey; //Set main function scope
 
 var getdata = function () {
     console.log("Updating Data");
@@ -144,6 +103,8 @@ var getdata = function () {
     });
 };
 $( document ).ready(function() {
+    sitecode = $.QueryString["code"];
+    apikey = $.QueryString["key"];
     if (sitecode.length <6 || apikey.length <1 ) {
         bootbox.dialog({
             message: '<p class="text-center">Please pass a code and api key as get parameters - see <a target="_blank" href="https://github.com/Jbithell/SolarEdgeESign/blob/master/README.md">documentation</a></p>',
