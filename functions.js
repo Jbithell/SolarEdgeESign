e8f835f170e6ded3cc7df2d48fa17c7b540f5fce
James Bithell <hi@jbithell.com>
2017-07-19 21:58:03 +0100

    Seperate functions into seperate file

---
+++ functions.js
@@ -0,0 +1,37 @@
+//Get function
+(function($) {
+    $.QueryString = (function(paramsArray) {
+        let params = {};
+
+        for (let i = 0; i < paramsArray.length; ++i)
+        {
+            let param = paramsArray[i]
+                .split('=', 2);
+
+            if (param.length !== 2)
+                continue;
+
+            params[param[0]] = decodeURIComponent(param[1].replace(/\+/g, " "));
+        }
+
+        return params;
+    })(window.location.search.substr(1).split('&'))
+})(jQuery);
+
+//Rounding Function
+function round(value, precision) {
+    var multiplier = Math.pow(10, precision || 0);
+    return Math.round(value * multiplier) / multiplier;
+}
+
+//Data conversion function
+var convertdata = function(data) {
+    if (data < 1001) {
+        return round(data,0) + "Wh";
+    } else if (data > 1000000) {
+        return round(data/1000000,1) + "MWh";
+    } else {
+        return round(data/1000,1) + "kWh";
+    }
+};
+
